cf8ff911bfec8a0255032f612e37ee18dedfd52f
guillermo.gonzalez@sdggroup.com <guillermo.gonzalez@sdggroup.com>
2022-03-23 11:01:21 +0000

    create customers

--- models/dim_customers.sql
+++ models/dim_customers.sql
@@ -2,27 +2,12 @@
     materializad="table"
 )}}
 
-with customers as (
-
-    select
-        id as customer_id,
-        first_name,
-        last_name
-
-    from raw.jaffle_shop.customers
-
+with customers as(
+    select * from {{ref('stg_customers') }}
 ),
 
-orders as (
-
-    select
-        id as order_id,
-        user_id as customer_id,
-        order_date,
-        status
-
-    from raw.jaffle_shop.orders
-
+orders as(
+    select * from {{ref('stg_orders') }}
 ),
 
 customer_orders as (
